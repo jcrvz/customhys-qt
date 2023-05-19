bf8e255c41d450b0ea9983bf5ef546185eb79f5d
Jorge Mario Cruz-Duarte <jorge.cruz@itesm.mx>
2023-05-19 17:10:25 -0600

    dist

---
+++ create-dmg.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Create a folder (named dmg) to prepare our DMG in (if it doesn't already exist).
+mkdir -p dist/dmg
+# Empty the dmg folder.
+rm -r dist/dmg/*
+# Copy the app bundle to the dmg folder.
+cp -r "dist/qustomhys.app" dist/dmg
+# If the DMG already exists, delete it.
+test -f "dist/qustomhys.dmg" && rm "dist/qustomhys.dmg"
+create-dmg \
+  --volname "qustomhys" \
+  --volicon "data/chm_logo.icns" \
+  --window-pos 200 120 \
+  --window-size 600 300 \
+  --icon-size 100 \
+  --icon "qustomhys.app" 175 120 \
+  --hide-extension "qustomhys.app" \
+  --app-drop-link 425 120 \
+  "dist/qustomhys.dmg" \
+  "dist/dmg/"
